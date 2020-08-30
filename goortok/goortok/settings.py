818e169167b4927060cc86cf6a82f9a45402cc37
akarev0 <akarev0@github.com>
2020-08-30 20:01:35 +0300

    update settings

--- goortok/goortok/settings.py
+++ goortok/goortok/settings.py
@@ -19,12 +19,14 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9ohryztdbe#vi*a^_)t6hn0^ue$55lo9yk!2g02=czis-2%#r1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'goortok-art.herokuapp.com',
+]
 
 # Application definition
 
